8357c757dcdf553cf557ed3bbb76b9757d58b37c
Viacheslav Orlov <viacheslavorlov@gmail.com>
2023-09-23 22:01:55 +0300

    throttle.ts tipization

--- src/shared/lib/throttle/throttle.ts
+++ src/shared/lib/throttle/throttle.ts
@@ -1,6 +1,7 @@
-export const throttle = (callback, delay) => {
-    let timerId;
-    return function (...args) {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const throttle = (callback: (...args: any[]) => void, delay: number): ((...args: any[]) => void) => {
+    let timerId: NodeJS.Timeout | null;
+    return function (this: any, ...args: any[]) {
         if (!timerId) {
             timerId = setTimeout(() => {
                 callback.apply(this, args);
